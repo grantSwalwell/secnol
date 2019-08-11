1b39d6d7c2a6453e321373cd5cb2d4be7992fc7e
grantSwalwell <grant.swalwell-1@ou.edu>
2019-08-11 13:51:48 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import logo from './logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Secret Knowledge Mentoring
+        </p>
+        <e>
+          ulysses@secretknowledge.io
+        </e>
+
+        <div class="p p-1"></div>
+        <div class="p p-2"></div>
+        <div class="p p-3"></div>
+        <div class="p p-4"></div>
+        <div class="p p-5"></div>
+        <div class="p p-6"></div>
+        <div class="p p-7"></div>
+        <div class="p p-8"></div>
+        <div class="p p-9"></div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
