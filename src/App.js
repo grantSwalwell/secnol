d2ea2fa2cf287c504dda9b31102bf968000c51c2
grantSwalwell <grant.swalwell-1@ou.edu>
2019-08-18 12:24:56 -0500

    basic update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
 import React from 'react';
 import './App.css';
+import './animation_vertical_down.css'
+import './animation_vertical_up.css'
+import './animation_horizontal_right.css'
+import './animation_horizontal_left.css'
 import logo from './logo.png';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} className="App-logo" alt="logo" align="left"/>
+        <div class="cross-borders"></div>
+        <h1>
           Secret Knowledge Mentoring
-        </p>
+        </h1>
         <e>
           ulysses@secretknowledge.io
         </e>
 
-        <div class="p p-1"></div>
-        <div class="p p-2"></div>
-        <div class="p p-3"></div>
-        <div class="p p-4"></div>
-        <div class="p p-5"></div>
-        <div class="p p-6"></div>
-        <div class="p p-7"></div>
-        <div class="p p-8"></div>
-        <div class="p p-9"></div>
+        <div class="pvd pvd-1"></div>
+        <div class="pvd pvd-2"></div>
+        <div class="pvd pvd-3"></div>
+        <div class="pvd pvd-4"></div>
+        <div class="pvd pvd-5"></div>
+        <div class="pvd pvd-6"></div>
+        <div class="pvd pvd-7"></div>
+        <div class="pvd pvd-8"></div>
+        <div class="pvd pvd-9"></div>
+
+        <div class="pvu pvu-1"></div>
+        <div class="pvu pvu-2"></div>
+        <div class="pvu pvu-3"></div>
+        <div class="pvu pvu-4"></div>
+        <div class="pvu pvu-5"></div>
+        <div class="pvu pvu-6"></div>
+        <div class="pvu pvu-7"></div>
+        <div class="pvu pvu-8"></div>
+        <div class="pvu pvu-9"></div>
+
+        <div class="pvr pvr-1"></div>
+        <div class="pvr pvr-2"></div>
+        <div class="pvr pvr-3"></div>
+        <div class="pvr pvr-4"></div>
+        <div class="pvr pvr-5"></div>
+        <div class="pvr pvr-6"></div>
+        <div class="pvr pvr-7"></div>
+        <div class="pvr pvr-8"></div>
+        <div class="pvr pvr-9"></div>
+
+        <div class="pvl pvl-1"></div>
+        <div class="pvl pvl-2"></div>
+        <div class="pvl pvl-3"></div>
+        <div class="pvl pvl-4"></div>
+        <div class="pvl pvl-5"></div>
+        <div class="pvl pvl-6"></div>
+        <div class="pvl pvl-7"></div>
+        <div class="pvl pvl-8"></div>
+        <div class="pvl pvl-9"></div>
         
       </header>
     </div>
